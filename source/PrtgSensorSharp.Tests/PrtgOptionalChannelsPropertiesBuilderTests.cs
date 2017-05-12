dea747c10961ad17dac1578a463e67ab16d697eb
Przemyslaw Polanski <przemyslaw.polanski@btskyrise.com>
2017-05-12 15:56:13 +0200

    Added OptionalChannelPropertiesBuilder

---
+++ source/PrtgSensorSharp.Tests/PrtgOptionalChannelsPropertiesBuilderTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PrtgSensorSharp.Tests
+{
+    public class PrtgOptionalChannelsPropertiesBuilderTests
+    {
+        [Test]
+        public void can_build_optional_properties()
+        {
+            var sut = new PrtgOptionalChannelPropertiesBuilder()
+                .WithLimitMaxWarning(10)
+                .WithLimitMaxError(100.1M)
+                .WithLimitMinError(-99.991M)
+                .WithLimitMinWarning(0)
+                .WithLimitsEnabled();
+                
+            var result = sut.Build();
+
+            result.Length.ShouldBeEquivalentTo(5);
+
+            result.Should().Contain(p => p.Name == "LimitMode" && p.Value == "1");
+            result.Should().Contain(p => p.Name == "LimitMaxError" && p.Value == "100.1");
+            result.Should().Contain(p => p.Name == "LimitMaxWarning" && p.Value == "10");
+            result.Should().Contain(p => p.Name == "LimitMinError" && p.Value == "-99.991");
+            result.Should().Contain(p => p.Name == "LimitMinWarning" && p.Value == "0");
+
+        }
+
+        [Test]
+        public void can_create_LimitMode_property()
+        {
+            var sut = new PrtgOptionalChannelPropertiesBuilder()
+                .WithLimitsEnabled();
+
+            var result = new PrtgResult(channel: "TestChannel", value: 1, optionalProperties: sut.Build());
+
+            result.Serialize().ToString().Should().Contain("<LimitMode>1</LimitMode>");
+        }
+        [Test]
+        public void can_create_LimitMaxError_property()
+        {
+            var sut = new PrtgOptionalChannelPropertiesBuilder()
+                .WithLimitMaxError(100.923m);
+
+            var result = new PrtgResult(channel: "TestChannel", value: 1, optionalProperties: sut.Build());
+
+            result.Serialize().ToString().Should().Contain("<LimitMaxError>100.923</LimitMaxError>");
+        }
+
+        [Test]
+        public void can_create_LimitMaxWarning_property()
+        {
+            var sut = new PrtgOptionalChannelPropertiesBuilder()
+                .WithLimitMaxWarning(10);
+
+            var result = new PrtgResult(channel: "TestChannel", value: 1, optionalProperties: sut.Build());
+
+            result.Serialize().ToString().Should().Contain("<LimitMaxWarning>10</LimitMaxWarning>");
+        }
+
+        [Test]
+        public void can_create_LimitMinError_property()
+        {
+            var sut = new PrtgOptionalChannelPropertiesBuilder()
+                .WithLimitMinError(-100.234m);
+
+            var result = new PrtgResult(channel: "TestChannel", value: 1, optionalProperties: sut.Build());
+
+            result.Serialize().ToString().Should().Contain("<LimitMinError>-100.234</LimitMinError>");
+        }
+
+        [Test]
+        public void can_create_LimitMinWarning_property()
+        {
+            var sut = new PrtgOptionalChannelPropertiesBuilder()
+                .WithLimitMinWarning(999999999999999999999999.9999m);
+
+            var result = new PrtgResult(channel: "TestChannel", value: 1, optionalProperties: sut.Build());
+
+            result.Serialize().ToString().Should().Contain("<LimitMinWarning>999999999999999999999999.9999</LimitMinWarning>");
+        }
+    }
+}
---
+++ source/PrtgSensorSharp/PrtgOptionalChannelPropertiesBuilder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace PrtgSensorSharp
+{
+    /// <summary>
+    /// This class makes it easy to create most common optional properties for the prtg channel. It will be probably extended with all available optional properties in the future
+    /// </summary>
+    public class PrtgOptionalChannelPropertiesBuilder
+    {
+        private readonly List<PrtgOptionalChannelProperty> _properties = new List<PrtgOptionalChannelProperty>();
+
+        /// <summary>
+        /// Define if the limit settings will be active. By default they are disabled. This setting will be considered only on the first sensor scan, when the channel is newly created; it is ignored on all further sensor scans (and may be omitted). You can change this initial setting later in the Channel settings of the sensor.
+        /// </summary>
+        /// <returns></returns>
+        public PrtgOptionalChannelPropertiesBuilder WithLimitsEnabled()
+        {
+            _properties.Add(new PrtgOptionalChannelProperty("LimitMode", "1"));
+            return this;
+        }
+
+        /// <summary>
+        /// The Limit will not take effect in the LimitMode is not set to 1. To set the LimitMode to 1 use the <see cref="WithLimitsEnabled"/> method
+        /// Define an upper error limit for the channel. If enabled, the sensor will be set to a "Down" status if this value is overrun and the LimitMode is activated. Note: Please provide the limit value in the unit of the base data type, just as used in the Value of the result. While a sensor shows a "Down" status triggered by a limit, it will still receive data in its channels. The values defined with this element will be considered only on the first sensor scan, when the channel is newly created; they are ignored on all further sensor scans (and may be omitted). You can change this initial setting later in the Channel settings of the sensor.
+        /// </summary>
+        /// <param name="max">The upper error limit.</param>
+        /// <returns></returns>
+        public PrtgOptionalChannelPropertiesBuilder WithLimitMaxError(decimal max)
+        {
+            _properties.Add(new PrtgOptionalChannelProperty("LimitMaxError", max.ToString(CultureInfo.InvariantCulture)));
+            return this;
+        }
+
+        /// <summary>
+        /// The Limit will not take effect in the LimitMode is not set to 1. To set the LimitMode to 1 use the <see cref="WithLimitsEnabled"/> method
+        /// Define an upper warning limit for the channel. If enabled, the sensor will be set to a "Warning" status if this value is overrun and the LimitMode is activated. Note: Please provide the limit value in the unit of the base data type, just as used in the the Value of the result. The values defined with this element will be considered only on the first sensor scan, when the channel is newly created; they are ignored on all further sensor scans (and may be omitted). You can change this initial setting later in the Channel settings of the sensor.
+        /// </summary>
+        /// <param name="max">The upper warning limit</param>
+        /// <returns></returns>
+        public PrtgOptionalChannelPropertiesBuilder WithLimitMaxWarning(decimal max)
+        {
+            _properties.Add(new PrtgOptionalChannelProperty("LimitMaxWarning", max.ToString(CultureInfo.InvariantCulture)));
+            return this;
+        }
+
+        /// <summary>
+        /// The Limit will not take effect in the LimitMode is not set to 1. To set the LimitMode to 1 use the <see cref="WithLimitsEnabled"/> method
+        /// Define a lower warning limit for the channel. If enabled, the sensor will be set to a "Warning" status if this value is undercut and the LimitMode is activated. Note: Please provide the limit value in the unit of the base data type, just as used in the Value of the result. The values defined with this element will be considered only on the first sensor scan, when the channel is newly created; they are ignored on all further sensor scans (and may be omitted). You can change this initial setting later in the Channel settings of the sensor.
+        /// </summary>
+        /// <param name="min">The lower warning limit.</param>
+        /// <returns></returns>
+        public PrtgOptionalChannelPropertiesBuilder WithLimitMinWarning(decimal min)
+        {
+            _properties.Add(new PrtgOptionalChannelProperty("LimitMinWarning", min.ToString(CultureInfo.InvariantCulture)));
+            return this;
+        }
+
+        /// <summary>
+        /// The Limit will not take effect in the LimitMode is not set to 1. To set the LimitMode to 1 use the <see cref="WithLimitsEnabled"/> method
+        /// Define a lower error limit for the channel. If enabled, the sensor will be set to a "Down" status if this value is undercut and the LimitMode is activated. Note: Please provide the limit value in the unit of the base data type, just as used in the Value of the result. While a sensor shows a "Down" status triggered by a limit, it will still receive data in its channels. The values defined with this element will be considered only on the first sensor scan, when the channel is newly created; they are ignored on all further sensor scans (and may be omitted). You can change this initial setting later in the Channel settings of the sensor.
+        /// </summary>
+        /// <param name="min">The lower error limit.</param>
+        /// <returns></returns>
+        public PrtgOptionalChannelPropertiesBuilder WithLimitMinError(decimal min)
+        {
+            _properties.Add(new PrtgOptionalChannelProperty("LimitMinError", min.ToString(CultureInfo.InvariantCulture)));
+            return this;
+        }
+
+        public PrtgOptionalChannelProperty[] Build()
+        {
+            return _properties.ToArray();
+        }
+    }
+}
